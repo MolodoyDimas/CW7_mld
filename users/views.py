a1aacc0823c9946023a02b051c2651346ee17264
Dmitry <kip@gmail.com>
2024-02-06 23:49:18 +0700

    изсправления в views.UserViewSet

--- users/views.py
+++ users/views.py
@@ -1,14 +1,32 @@
 from rest_framework.permissions import AllowAny
 from rest_framework.viewsets import ModelViewSet
-
+from rest_framework import viewsets, status
 from users.models import User
 from users.serializers import UserSerializer
+from requests import Response
 
-
-class UserViewSet(ModelViewSet):
+class UserViewSet(viewsets.ModelViewSet):
     """
     Набор представлений для пользовательской модели.
     """
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-    permission_classes = [AllowAny]
+
+    def create(self, request):
+        serializer = UserSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+
+        password = serializer.data["password"]
+        user = User.objects.get(pk=serializer.data["id"])
+        user.set_password(password)
+        user.save()
+
+        return Response(
+            serializer.data,
+            status=status.HTTP_201_CREATED,
+            headers=headers
+        )
+
+    #queryset = User.objects.all()
+    #serializer_class = UserSerializer
+    #permission_classes = [AllowAny]
