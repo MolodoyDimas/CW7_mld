fee85b201737c45f2f8308c0618a032b8abeb97e
Dmitry <kip@gmail.com>
2024-02-11 20:55:57 +0700

    Исправления работы UserViewSet()

--- users/views.py
+++ users/views.py
@@ -3,13 +3,14 @@ from rest_framework.viewsets import ModelViewSet
 from rest_framework import viewsets, status
 from users.models import User
 from users.serializers import UserSerializer
-from requests import Response
+from rest_framework.response import Response
 
 class UserViewSet(viewsets.ModelViewSet):
     """
     Набор представлений для пользовательской модели.
     """
-
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
     allowe_http_methods = ["post"] #подсказка
     permisson_class = [AllowAny] #подсказка
 
@@ -32,6 +33,6 @@ class UserViewSet(viewsets.ModelViewSet):
             headers=headers
         )
 
-    #queryset = User.objects.all()
+
     #serializer_class = UserSerializer
     #permission_classes = [AllowAny]
